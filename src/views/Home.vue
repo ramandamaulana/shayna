44bb27954191e2a9e6bea6af4bb833cb07488f8d
Ram.id <masramanda@gmail.com>
2020-04-30 14:12:56 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <ContentShayna />
+    <BannerShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import ContentShayna from "@/components/ContentShayna.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    ContentShayna,
+    BannerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+};
+</script>
